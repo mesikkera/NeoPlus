efbfcda9477362ec4ac2c9343823c6df5eb2d2c2
mesikkera <mesikkera@gmail.com>
2018-11-02 17:03:50 +0900

    cpp/header 파일 분리

--- MessagePacket.cpp
+++ MessagePacket.cpp
@@ -2,52 +2,48 @@
  *	MessagePacket.cpp
  */
 
+#include "MessagePacket.h"
+
 #include <stdint.h>
-#include <string>
-#include <google/protobuf/message.h>
 
-struct PacketHeader {
-	int32_t size;					// 패킷 전체 사이즈 (메세지 패킷 + 패킷 헤더)
-	int32_t encoding;				// 패킷 데이터 인코딩 (헤더 제외)
-	int32_t type;					// 메세지 타입
-	int32_t reserved;
-};
+namespace neoplus {
 
-enum { PacketHeaderSize = sizeof(PacketHeader) };
+	static inline void Int32ToBytes(int32_t n, char bytes[]) {
+		bytes[0] = (n >> 24) & 0xff;
+		bytes[1] = (n >> 16) & 0xff;
+		bytes[2] = (n >> 8) & 0xff;
+		bytes[3] = (n >> 0) & 0xff;
+	}
 
-enum PacketType {
-	RequestPacket,
-	ResponsePacket, 
-	NotificationPacket,
-	AcknowledgementPacket
-};
+	static inline int32_t Int32FromByte(uint8_t bytes[]) {
+		return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+	}
 
-static inline void Int32ToBytes(int32_t n, char bytes[]) {
-	bytes[0] = (n >> 24) & 0xff;
-	bytes[1] = (n >> 16) & 0xff;
-	bytes[2] = (n >> 8) & 0xff;
-	bytes[3] = (n >> 0) & 0xff;
-}
+	::std::string PacketFromMessage(const ::google::protobuf::Message &message, PacketType type)
+	{
+		int size = message.ByteSize() + PacketHeaderSize;
+		const int encoding = 0;
+		char header[PacketHeaderSize];
+		Int32ToBytes(size, header + offsetof(PacketHeader, size));
+		Int32ToBytes(encoding, header + offsetof(PacketHeader, type));
+		Int32ToBytes(type, header + offsetof(PacketHeader, type));
+		Int32ToBytes(0, header + offsetof(PacketHeader, reserved));
+		std::string packet(header, sizeof(header));
+		message.AppendToString(&packet);
 
-inline int32_t Int32FromByte(uint8_t bytes[]) {
-	return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
-}
+		return packet;
+	}
 
-::std::string PacketFromMessage(const ::google::protobuf::Message &message, PacketType type)
-{
-	int size = message.ByteSize() + PacketHeaderSize;
-	const int encoding = 0;
-	char header[PacketHeaderSize];
-	Int32ToBytes(size, header + offsetof(PacketHeader, size));
-	Int32ToBytes(encoding, header + offsetof(PacketHeader, type));
-	Int32ToBytes(type, header + offsetof(PacketHeader, type));
-	Int32ToBytes(0, header + offsetof(PacketHeader, reserved));
-	std::string packet(header, sizeof(header));
-	message.AppendToString(&packet);
+	PacketHeader PacHeaderFromBytes(const void *data) { 
+		PacketHeader header;
+		const uint8_t *bytes = static_cast<const uint8_t *>(data);
 
-	return packet;
-}
+		header.size = Int32FromByte(bytes + offsetof(PacketHeader, size));
+		header.encoding = Int32FromByte(bytes + offsetof(PacketHeader, encoding));
+		header.type = Int32FromByte(bytes + offsetof(PacketHeader, type));
+		header.reserved = Int32FromByte(bytes + offsetof(PacketHeader, reserved));
 
-PacketHeader PacketHeaderFromBytes(const void *data) { 
-	const uint8_t *bytes = static_cast<const uint8_t *>(data);
-}
+		return header;
+	}
+}	// namespace neoplus
+
---
+++ MessagePacket.h
@@ -0,0 +1,29 @@
+#ifndef MESSAGEPACKET_H_
+#define MESSAGEPACKET_H_
+
+#include <stdint.h>
+#include <string>
+#include <google/protobuf/message.h>
+
+namespace neoplus {
+
+	struct PacketHeader {
+ 		int32_t size;				// 패킷 사이즈(헤더 포함)
+		int32_t encoding;			// 패킷 데이터 인코딩(헤더 제외)
+		int32_t type;				// 패킷 타입 (request, response, notification, acknowledgement)
+		int32_t reserved;
+	};
+
+	enum { PacketHeaderSize = sizeof(PacketHeader) };
+
+	enum PacketType {
+		RequestPacket, 
+		ResponsePacket,
+		NotificationPacket,
+		AcknowledgementPacket
+	};
+
+	::std::string PacketFromMessage(const ::google::protobuf::Message &message, PacketType type);
+	PacketHeader PacketHeaderFromBytes(const void *data);
+}
+#endif	// MESSAGEPACKET_H_
