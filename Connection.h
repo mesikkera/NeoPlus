abe2812f4fce70164c9aa61f57b8c638da9df556
mesikkera <mesikkera@gmail.com>
2018-11-07 18:48:35 +0900

    Connection 모듈 파일 생성 및 구현 시작

---
+++ Connection.h
@@ -0,0 +1,18 @@
+#ifndef CONNECTION_H_
+#define CONNECTION_H_
+
+#include <boost/asio.hpp>
+
+class Connection {
+private:
+	boost::asio::ip::tcp::socket socket;
+
+	// 응답 처리(from Server)
+
+	// 요청 처리(to Server)
+
+public:
+	Connection();
+};
+
+#endif
