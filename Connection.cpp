6f13708b5e1e31e34c78db00b915e11dda10ec19
mesikkera <mesikkera@gmail.com>
2018-11-15 16:21:26 +0900

    Connection 생성자 및 close 메소드 구현

--- Connection.cpp
+++ Connection.cpp
@@ -1,15 +1,31 @@
 #include "Connection.h"
 
+#include <boost/bind.hpp>
+
+using boost::asio::ip::tcp;
+
 namespace neoplus {
 
-	// class Connection {
-	// private:
-	// 	boost::asio::ip::tcp::socket socket;
+	Connection::Connection(boost::asio::io_service& ios,
+						   tcp::resolver::iterator iter) : _strand(ios), _socket(ios), _iterator(iter) {
+		boost::asio::async_connect(_socket, _iterator, _strand);
 
-	// public:
-	// 	Connection(boost::asio::io_service& io_service) : socket(io_service) {
+	}
 
-	// 	}
-	// };
-	Connection::Connection(boost::asio::io_service& io_service);
+	// TODO: closing의 적절한 이름 다시 생각해볼것.
+	// closeSocket()
+	void Connection::closeSocket() {
+		if (_socket.is_open()) {
+			boost::system::error_code error;
+			_socket.close(error);
+		}
+	}
+
+	void Connection::close() {
+		_strand.post(boost::bind(&Connection::closeSocket, this));
+	}
+
+	void Connection::sendRequest(message_ptr request) {
+		_strand.post(boost::bind(&Connection::closeSocket, this, request));
+	}
 }
--- Connection.h
+++ Connection.h
@@ -6,10 +6,24 @@
 #include <boost/asio.hpp>
 
 namespace neoplus {
+
+using message_ptr = boost::shared_ptr<google::protobuf::Message>;
 	
 	class Connection {
 	public:
-		Connection(boost::asio::io_service& io_service);
+		Connection(boost::asio::io_service& ios,
+				   boost::asio::ip::tcp::resolver::iterator iter);
+		
+		void close();
+		void sendRequest(message_ptr request);
+		
+	private:
+		boost::asio::io_service::strand _strand;
+		boost::asio::ip::tcp::socket _socket;
+		boost::asio::ip::tcp::resolver::iterator _iterator;
+
+		void closeSocket();
+
 	};
 }
 
