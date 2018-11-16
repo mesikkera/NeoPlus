d97273f7e74aebde1167392155cde34872dd48f5
mesikkera <mesikkera@gmail.com>
2018-11-16 17:26:15 +0900

    request sending method 구현

--- Connection.cpp
+++ Connection.cpp
@@ -12,20 +12,46 @@ namespace neoplus {
 
 	}
 
-	// TODO: closing의 적절한 이름 다시 생각해볼것.
-	// closeSocket()
-	void Connection::closeSocket() {
+	void Connection::close() {
+		_strand.post(boost::bind(&Connection::preClose, this));
+	}
+
+	void Connection::preClose() {
 		if (_socket.is_open()) {
 			boost::system::error_code error;
 			_socket.close(error);
 		}
 	}
 
-	void Connection::close() {
-		_strand.post(boost::bind(&Connection::closeSocket, this));
-	}
 
 	void Connection::sendRequest(message_ptr request) {
-		_strand.post(boost::bind(&Connection::closeSocket, this, request));
+		_strand.post(boost::bind(&Connection::sendRequest, this, request));
 	}
+
+	void Connection::preSendRequest(message_ptr request) {
+		bool idle = requestQueue.empty();
+		requestQueue.push_back(neoplus::PacketFromMessage(*request, neoplus::RequestPacket));
+		if(idle) {
+			boost::asio::async_write(_socket, boost::asio::buffer(requestQueue.front()), 
+									 _strand.wrap(boost::bind(&Connection::handleSendQueuedRequest, this, boost::asio::placeholders::error)));
+		}		
+
+	}
+
+	void Connection::handleSendQueuedRequest(const boost::system::error_code &error) {
+		if (!error) {
+			requestQueue.pop_front();
+			if(!requestQueue.empty()) {
+				// send request in queue
+			}
+		} else {
+			preClose();
+		}
+	}
+
+	void Connection::sendQueuedRequest() {
+		boost::asio::async_write(_socket, boost::asio::buffer(requestQueue.front()), 
+								 _strand.wrap(boost::bind(&Connection::handleSendQueuedRequest, this, boost::asio::placeholders::error)));
+	}
+
 }
--- Connection.h
+++ Connection.h
@@ -4,6 +4,7 @@
 #include "MessagePacket.h"
 
 #include <boost/asio.hpp>
+#include <deque>
 
 namespace neoplus {
 
@@ -16,13 +17,22 @@ using message_ptr = boost::shared_ptr<google::protobuf::Message>;
 		
 		void close();
 		void sendRequest(message_ptr request);
-		
+
 	private:
 		boost::asio::io_service::strand _strand;
 		boost::asio::ip::tcp::socket _socket;
 		boost::asio::ip::tcp::resolver::iterator _iterator;
 
-		void closeSocket();
+		std::deque<std::string> requestQueue;
+
+		void preClose();
+		
+		// request to server.
+		void handleSendQueuedRequest(const boost::system::error_code &error);
+		void sendQueuedRequest();
+		void preSendRequest(message_ptr request);
+
+		// response from server.
 
 	};
 }
