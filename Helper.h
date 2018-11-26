c8c2a869a53c546f271ba90747f3fa337efa1ab9
mesikkera <mesikkera@gmail.com>
2018-11-26 20:44:30 +0900

    Helper 파일 생성 및 compareVersion() 구현 시작

---
+++ Helper.h
@@ -0,0 +1,18 @@
+//
+//  Helper.h
+//  NeoPlus
+//
+//  Created by mesikkera on 26/11/2018.
+//  Copyright © 2018 mesikkera. All rights reserved.
+//
+
+#ifndef HELPER_H_
+#define HELPER_H_
+
+#include <string>
+
+namespace neoplus {
+	bool CompareVersion(const std::string &version1, const std::string &version2);
+}
+
+#endif	// HELPER_H_
