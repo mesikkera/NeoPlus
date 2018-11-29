5e5f8caf11da7bedd29919f93920332ba699b40e
mesikkera <mesikkera@gmail.com>
2018-11-29 15:30:48 +0900

    Session 파일 생성

---
+++ Session.h
@@ -0,0 +1,14 @@
+//
+//  Session.h
+//  NeoPlus
+//
+//  Created by mesikkera on 29/11/2018.
+//  Copyright © 2018 mesikkera. All rights reserved.
+//
+
+namespace neoplus {
+
+	class Session {
+
+	};
+}
